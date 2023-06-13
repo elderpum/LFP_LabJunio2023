c094edfa43f04255a5d7bcf3a79319c8abc5ba17
Elder Pum <ElderPum@gmail.com>
2023-06-13 17:53:52 -0600

    Ejemplo 4

---
+++ Ejemplo 4 - Ejecutable/main.py
@@ -0,0 +1,8 @@
+# Libreria a utilizar: pyinstaller
+
+salir = False
+while (not salir):
+    respuesta = input("Desea cerrar esta ventana? (Si/No) ")
+    if (respuesta == "Si"):
+        salir = True
+        print("Adios :c")
